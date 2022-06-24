b05d2731e252d89ba42c20f61df0640b3b559c11
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-24 15:53:24 -0300

    first commit

---
+++ pages/api/profiles.js
@@ -0,0 +1,13 @@
+const handler = (req, res) => {
+  try {
+    res.status(200).json([
+      { id: 1, name: 'Gabriel', email: 'gabriel@gmail.com' },
+      { id: 2, name: 'Zanin', email: 'zanin@gmail.com' },
+      { id: 2, name: 'Gabriel Zanin', email: 'gabrielzanin@gmail.com' },
+    ]);
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
+};
+
+export default handler;
